8c1bdcca2bc27a4cee9aa05e5c53294a191521c3
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-04-30 16:59:26 -0300

    Modifiqué el formato de la respuesta de createOrderMERCADOPAGO para poder acceder response.url

--- controllers/mercadopago.controller.js
+++ controllers/mercadopago.controller.js
@@ -22,7 +22,7 @@ mercadopago.configure({
             },
         ]
     });
-    res.json(result.body.init_point);
+    res.json({"url":result.body.init_point});
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something goes wrong Juancito" });
