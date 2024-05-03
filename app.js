a6a068320b2c443c7de7ab6023c7d4ea5af27cd1
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-05-03 15:25:35 -0300

    modificación total del socket chat

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 require('dotenv').config();
 const { Chat, Demand, User } = require('./models');
 
+
 const port = process.env.PORT || 8080;
 const Routes = require('./routes/index');
 const { conn } = require('./db/config');
@@ -33,56 +34,5 @@ const io = new Server(servidor, {
 });
 
 io.on('connection', (socket) => {
-  //console.log('Connected to socket.io');
-  socket.on('setup', (userData) => {
-    //console.log(userData);
-    socket.join(userData.uid);
-    socket.emit('connected');
-  });
-
-  socket.on('join chat', (room) => {
-    socket.join(room);
-  });
-  socket.on('typing', (room) => socket.in(room).emit('typing'));
-  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
-
-  socket.on('new message', async (newMessageRecieved) => {
-    //console.log(newMessageRecieved);
-    var chatId = newMessageRecieved.chat;
-    // var sender = newMessageRecieved.sender;
-    // const chat = await Chat.findById(chatId);
-    //console.log(chat);
-
-    // if (sender == chat.seller) {
-    //   socket.in(chat.buyer).emit('message recieved', newMessageRecieved);
-    // } else {
-    //   socket.in(chat.seller).emit('message recieved', newMessageRecieved);
-    // }
-    socket.to(chatId).emit('message recieved', newMessageRecieved);
-  });
-  socket.on('new demand', async (newDemandRecieved) => {
-    const sellers = await User.find({ role: 'SELLER_ROLE' });
-    // sellers.forEach((user) => {
-    //   socket.in(user._id).emit('new demand recieved', newDemandRecieved);
-    // });
-    socket.to('demandas').emit('new demand recieved', newDemandRecieved);
-  });
-
-  socket.on('new offer', async (newOffer) => {
-    var demandId = newOffer.demand;
-    const demand = await Demand.findById(demandId);
-    const user = demand.user;
-    //socket.in(user).emit('new offer recieved', newOffer);
-    socket.to(demandId).emit('new offer recieved', newOffer);
-  });
-
-  socket.on('new order', async (newOrder) => {
-    var seller = newOrder.seller;
-    socket.in(seller).emit('new order recieved', newOffer);
-  });
-
-  socket.off('setup', () => {
-    //console.log('USER DISCONNECTED');
-    socket.leave(userData._id);
-  });
+console.log('Conectado juancito ')
 });
