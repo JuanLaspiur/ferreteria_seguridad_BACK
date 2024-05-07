3b5d0c5b9363ec68ab827918a8dbd08eee87d679
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-05-07 15:55:27 -0300

    Modificación createOrderMERCADOPAGO PARA MÁS DE UN PRODUCTO

--- controllers/mercadopago.controller.js
+++ controllers/mercadopago.controller.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 mercadopago.configure({
   access_token: `${process.env.MPAGO_TOKEN}`
 });
-
+/*
  const createOrderMERCADOPAGO = async (req, res) => {
   try { 
     const { user, name, category, price, quantity } = req.body;
@@ -25,7 +25,29 @@ mercadopago.configure({
     console.error(error);
     res.status(500).json({ message: "Something goes wrong" });
   }
+}; */
+const createOrderMERCADOPAGO = async (req, res) => {
+  try { 
+    const items = req.body.map(product => ({
+      id: product.user,
+      description: product.category,
+      title: product.name,
+      unit_price: product.price,
+      currency_id: "CLP",
+      quantity: product.quantity,
+    }));
+
+    const result = await mercadopago.preferences.create({
+      items: items
+    });
+
+    res.json({"url": result.body.init_point});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
+
 module.exports = {
   createOrderMERCADOPAGO
 };
